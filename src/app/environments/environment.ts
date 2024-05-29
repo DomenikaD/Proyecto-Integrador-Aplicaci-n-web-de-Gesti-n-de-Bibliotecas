fab9930953243b8b945d437543c87fadceadd344
Dome Delgado <ddelgadoi@est.ups.edu.ec>
2024-05-29 17:42:08 -0500

    Conexión con FireBase & Creación de Componete Libro

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyDF3Uxq6jWLbsYDLdHbojGNnFXsTDeDd8U",
+        authDomain: "biblioteca-60b30.firebaseapp.com",
+        projectId: "biblioteca-60b30",
+        storageBucket: "biblioteca-60b30.appspot.com",
+        messagingSenderId: "350057523933",
+        appId: "1:350057523933:web:c45b735ef8cde4d1bd4934"
+      
+    }
+  };
+
+  
